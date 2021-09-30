5f0ced38f992a05a068ff62f56e52a048fb3a4ff
Burucs Adam <burucs.adam@nexius.hu>
2021-09-30 14:12:47 +0200

    Add describe part for System Service test

--- src/services/system.service.spec.ts
+++ src/services/system.service.spec.ts
@@ -2,12 +2,14 @@ import SystemService from "./system.service";
 
 let systemService = new SystemService();
 
-test('the board is initialized', () => {
-    for(let i: number = 0; i < 5; i++) {
-        expect(systemService.board[i].length).toBe(5);
+describe('System Service', () => {
+  test('the board is initialized', () => {
+      for(let i: number = 0; i < 5; i++) {
+          expect(systemService.board[i].length).toBe(5);
 
-        for(let j: number = 0; j < 5; j++) {
-          expect(systemService.board[i][j].type).toBe("empty");
-        }
-    }
+          for(let j: number = 0; j < 5; j++) {
+            expect(systemService.board[i][j].type).toBe("empty");
+          }
+      }
+  });
 });
