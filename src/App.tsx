668b9fe5dc7d4a74dcab927ec12ee1bb85a2d5e2
Burucs Adam <burucs.adam@nexius.hu>
2021-10-11 14:33:15 +0200

    Add type to class state

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,33 @@ import React from 'react';
 import './App.scss';
 import SystemService from './services/system.service';
 
-class App extends React.Component {
+type MyState = { inputScript: string };
+
+class App extends React.Component<{}, MyState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      inputScript: "PLACE_ROBOT 3,3,NORTH\n" +
+        "PLACE_WALL 3,5\n" +
+        "MOVE\n" +
+        "MOVE\n" +
+        "RIGHT\n" +
+        "MOVE\n" +
+        "MOVE\n" +
+        "MOVE\n" +
+        "REPORT"
+    };
+    this.result = this.result.bind(this);
+  }
+
+  inputScriptChange = (event: any) => {
+    this.setState({ inputScript: event.target.value });
+  }
+
+  result(): void {
+    console.log(this.state.inputScript);
+  }
+
   render() {
     return (
       <div className="container">
@@ -10,22 +36,20 @@ class App extends React.Component {
           <h1 className="h1">Toy Robot App</h1>
           <h2 className="h2">Place the robot, build walls and move around!</h2>
           <img className="robot" src="robot.png" alt="Robot" />
+        </header>
+        <main className="main">
           <div className="form-container">
             <label htmlFor="command-script"></label>
-            <textarea className="textarea" name="" id="command-script">
-            </textarea>
+            <textarea className="textarea" name="" id="command-script" value={this.state.inputScript} onChange={this.inputScriptChange} />
           </div>
           <div className="form-container">
-            <button className="button">Get result</button>
+            <button className="button" onClick={this.result}>Get result</button>
           </div>
           <div className="form-container">
             Result:
             <div className="result"><code></code></div>
           </div>
-          <div>
-
-          </div>
-        </header>
+        </main>
         <footer className="footer">
           <p>Made in October, 2021.<br />
           Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p>
