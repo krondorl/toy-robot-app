0f6d88ca4473bf085d0752dc8c29da7363e2784f
Burucs Adam <burucs.adam@nexius.hu>
2021-10-11 11:14:53 +0200

    Add basic styles in SCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
+    <div className="app">
+      <header className="app-header">
+        <h1 className="h1">Toy Robot App</h1>
+        <img className="robot" src="robot.png" alt="" />
+        <div>
+          <label htmlFor="command-script"></label>
+          <textarea name="" id="command-script"></textarea>
+        </div>
+        <div>
+          <button>Get result</button>
+        </div>
+        <div>
+          Result:
+          <span className="result"></span>
+        </div>
+        <div>
+
+        </div>
       </header>
+      <footer>
+        <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a>
+        from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+      </footer>
     </div>
   );
 }
