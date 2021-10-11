6cd51818696e46bc31998b7a1f0053a10e81053a
Burucs Adam <burucs.adam@nexius.hu>
2021-10-11 13:22:32 +0200

    Convert App function to class component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
 import React from 'react';
 import './App.scss';
+import SystemService from './services/system.service';
 
-function App() {
-  return (
-    <div className="container">
-      <header className="app-header">
-        <h1 className="h1">Toy Robot App</h1>
-        <h2 className="h2">Place the robot, build walls and move around!</h2>
-        <img className="robot" src="robot.png" alt="" />
-        <div className="form-container">
-          <label htmlFor="command-script"></label>
-          <textarea className="textarea" name="" id="command-script">
-          </textarea>
-        </div>
-        <div className="form-container">
-          <button className="button">Get result</button>
-        </div>
-        <div className="form-container">
-          Result:
-          <div className="result"><code></code></div>
-        </div>
-        <div>
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <header className="app-header">
+          <h1 className="h1">Toy Robot App</h1>
+          <h2 className="h2">Place the robot, build walls and move around!</h2>
+          <img className="robot" src="robot.png" alt="Robot" />
+          <div className="form-container">
+            <label htmlFor="command-script"></label>
+            <textarea className="textarea" name="" id="command-script">
+            </textarea>
+          </div>
+          <div className="form-container">
+            <button className="button">Get result</button>
+          </div>
+          <div className="form-container">
+            Result:
+            <div className="result"><code></code></div>
+          </div>
+          <div>
 
-        </div>
-      </header>
-      <footer className="footer">
-        <p>Made in October, 2011.<br />
-        Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p>
-      </footer>
-    </div>
-  );
+          </div>
+        </header>
+        <footer className="footer">
+          <p>Made in October, 2021.<br />
+          Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p>
+        </footer>
+      </div>
+    );
+  }
 }
-
 export default App;
